d1eafde9c48a2f5bb09e4e04d4a46957dd6a82a9
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-04-05 11:15:40 +0900

    [ADD array remove]
    [ADD array remove]

--- src/App.js
+++ src/App.js
@@ -50,6 +50,10 @@ function App() {
       email: '',
     });
     NextId.current += 1;
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
   }
 
   return (
@@ -60,7 +64,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   )
 }
